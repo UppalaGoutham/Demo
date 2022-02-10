c775ba16f5696a251a086781b9d16aff86e9d9fe
UppalaGoutham <98812442+UppalaGoutham@users.noreply.github.com>
2022-02-10 21:06:16 +0530

    Add files via upload

---
+++ 3_Implementation/inc/ATM.h
@@ -0,0 +1,22 @@
+/**
+ * @file ATM.h
+ * @author goutham
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+ 
+#ifndef __ATM_H__
+#define __ATM_H__
+
+int Menu(void);
+float Balance(float balance);
+float Withdraw(float balance);
+float Deposit(float balance);
+int Exit(void);
+
+
+#endif
